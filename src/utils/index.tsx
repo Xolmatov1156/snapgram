f49bc66e2a0c64066eba3b394488e76ee03b53cb
Xolmatov1156 <xolmatov1156@gmail.com>
2024-10-14 14:04:09 +0500

    new

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,8 +1,17 @@
 import { Suspense } from "react"
+import { Triangle } from "react-loader-spinner"
 
 const Loading = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">Loading</div>
+    <div className="w-screen h-screen flex items-center justify-center bg-black"><Triangle
+    visible={true}
+    height="80"
+    width="80"
+    color="white"
+    ariaLabel="triangle-loading"
+    wrapperStyle={{}}
+    wrapperClass=""
+    /></div>
   )
 }
 
