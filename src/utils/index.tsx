8b59f3c0935bcf60d5aeeb101bebd4acc489b108
Xolmatov1156 <xolmatov1156@gmail.com>
2024-10-14 14:00:33 +0500

    new

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react"
 
 const Loading = () => {
   return (
-    <div>Loading</div>
+    <div className="w-screen h-screen flex items-center justify-center">Loading</div>
   )
 }
 
