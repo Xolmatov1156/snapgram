adeecbd7f9fe83c4961857600c9b95cd2560523b
Xolmatov1156 <xolmatov1156@gmail.com>
2024-10-14 14:08:53 +0500

    new

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,14 +2,16 @@ import { lazy, LazyExoticComponent } from "react";
 import { useRoutes } from "react-router-dom";
 import { SuspenseComponent as Suspense } from "../utils";
 
-const Home : LazyExoticComponent<any> = lazy(() => import('../pages/Login/Login'))
+const Login: LazyExoticComponent<any> = lazy(() => new Promise(resolve => {
+    setTimeout(() => resolve(import('../pages/Login/Login')), 1000);
+  }));
 const Register : LazyExoticComponent<any> = lazy(() => import('../pages/Register/Register'))
 
 const Routers = () => {
     return useRoutes ([
         {
             path: '/',
-            element: <Suspense><Home/></Suspense>
+            element: <Suspense><Login/></Suspense>
         },
         {
             path: '/register',
